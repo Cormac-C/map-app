8655349fbdf8cc2a52e1c4e8c36347841148750e
Cormac <cormaccureton@gmail.com>
2022-06-23 09:36:38 -0400

    Initial Map App

---
+++ src/components/Map.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { MapContainer, TileLayer, ZoomControl, AttributionControl} from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+
+export function Map() {
+    const zoom = 16.5;
+    const minZoom = 15;
+    const maxZoom = 18;
+    const zoomSnap = 0;
+    const zoomDelta = 0.25;
+    const currentLocation = { lat: 43.471, lng: -80.543 };
+    const mapBoxStyleUrl = "https://api.mapbox.com/styles/v1/cormacc/cl4pvmkgk000u15rpva8wyi39/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiY29ybWFjYyIsImEiOiJjbDRwdWQxcXIwNXJxM2NwbG9zcTg2cHkyIn0.yAYe13rux3sEtmH9ucEEpw"
+
+    return (
+        <>
+            <MapContainer
+                center={currentLocation}
+                zoom={zoom}
+                minZoom={minZoom}
+                maxZoom={maxZoom}
+                zoomSnap={zoomSnap}
+                zoomDelta={zoomDelta}
+                zoomControl={false}
+                attributionControl={false}
+                style = {{
+                height: "100vh",
+                width: "100vw",
+                }}
+            >
+                <TileLayer
+                    url={mapBoxStyleUrl}
+                />
+                <ZoomControl position="bottomleft" />
+                <AttributionControl position="bottomright" />
+            </MapContainer>
+        </>
+    )
+}
---
+++ src/pages/MainMap.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "leaflet/dist/leaflet.css";
+import { Map } from "../components/Map";
+
+
+export function MainMap() {
+  return (
+    <div className = "App">s
+      <Map/>
+    </div>
+  );
+}
