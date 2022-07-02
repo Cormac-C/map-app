65edac154a8803bb0a14be6a98cf00da63ce9e4d
Cormac <cormaccureton@gmail.com>
2022-07-02 12:30:48 -0400

    Add pins for all the measured areas

--- src/pages/MainMap.js
+++ src/pages/MainMap.js
@@ -5,14 +5,34 @@ import { Map } from "../components/Map";
 export function MainMap() {
   const studySpots = [
     {
-      "lat": 43.47146831296268, 
-      "lng": -80.54335554357826,
-      "title": "Rock Garden"
+      "lat": 43.47313806626149, 
+      "lng": -80.5403374368897, 
+      "title": "E7 SYDE Lounge"
     },
     {
-      "lat": 43.4728428044162, 
-      "lng": -80.54011120174852,
-      "title": "E7 6008 - AC"
+      "lat": 43.47307772562945,  
+      "lng": -80.54030256817363,
+      "title": "E7 6008"
+    },
+    {
+      "lat": 43.46950349322649, 
+      "lng": -80.54157509538742 ,
+      "title": "DP LIbrary Umbrellas"
+    },
+    {
+      "lat": 43.47029914832782, 
+      "lng": -80.54092536092635,
+      "title": "RCH 209"
+    },
+    {
+      "lat": 43.47248434388779,
+      "lng":  -80.54226019005233,
+      "title": "DC Silent Study"
+    },
+    {
+      "lat": 43.47135626330101, 
+      "lng": -80.54372532587942,
+      "title": "QNC 2501"
     }
   ]
   return (
